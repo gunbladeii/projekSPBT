31ebf073ead26a77fe9ba1e1a440dd4b825b9940
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-07-22 23:02:29 +0800

    Update index.php

--- adminSPBT/index.php
+++ adminSPBT/index.php
@@ -190,7 +190,7 @@ $totalRows_Recordset = mysqli_num_rows($Recordset);
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="dist/img/logo_kpm.png" alt="kpm_logo" class="brand-image img-circle elevation-3"
+      <img src="dist/img/logo_kpm.png" alt="kpm_logo" class="brand-image elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark">eSPBT2.0</span>
     </a>
