8ab8db35a02ad5a07faf0d626915eab4d144ed98
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-07-22 23:05:53 +0800

    Initial commit

--- adminSPBT/payroll.php
+++ adminSPBT/payroll.php
@@ -198,7 +198,7 @@ $mysqli->query("UPDATE `revenue` SET `grandTotal` = '$grandTotal2', `date` = '$d
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="dist/img/iberkat.jpeg" alt="altus Logo" class="brand-image img-circle elevation-3"
+      <img src="dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark">eSPBT2.0</span>
     </a>
--- adminSPBT/pod.php
+++ adminSPBT/pod.php
@@ -183,7 +183,7 @@ $totalRows_Recordset2 = mysqli_num_rows($Recordset2);
   <aside class="main-sidebar sidebar-light-primary elevation-4">
     <!-- Brand Logo -->
     <a href="index.php" class="brand-link">
-      <img src="dist/img/iberkat.jpeg" alt="altus Logo" class="brand-image img-circle elevation-3"
+      <img src="dist/img/logo_kpm.png" alt="altus Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
       <span class="brand-text font-weight-dark">eSPBT2.0</span>
     </a>
