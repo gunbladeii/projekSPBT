a0a232a56670189f58c7304c312c849d4b0687f2
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-07-26 18:50:46 +0800

    Update index.php

--- adminSPBT/index.php
+++ adminSPBT/index.php
@@ -366,7 +366,7 @@ $totalRows_Recordset = mysqli_num_rows($Recordset);
               </div>
             
           <div class="row">  
-            <div class="col-md-6"> 
+            <div class="col-md-12"> 
               <!-- TABLE: parcel delivery publisherSPBT -->
             <div class="card">
               <div class="card-header border-transparent">
@@ -394,33 +394,7 @@ $totalRows_Recordset = mysqli_num_rows($Recordset);
           </div>
           <!-- /.col -->        
       
-        <div class="col-md-6">
-           <!-- TABLE: list of odometer -->
-            <div class="card">
-              <div class="card-header border-transparent">
-                <h3 class="card-title">Senarai </h3>
-                <h2 class="card-title" style="font-size:14px;">(As of <?php echo $date.' '.$time;?>)</h2>
-
-                <div class="card-tools">
-                  <button type="button" class="btn btn-tool" data-widget="collapse">
-                    <i class="fas fa-minus"></i>
-                  </button>
-                  <button type="button" class="btn btn-tool" data-widget="remove">
-                    <i class="fas fa-times"></i>
-                  </button>
-                </div>
-              </div>
-              <!-- /.card-header -->
-              <div class="card-body p-0">
-                <div class="table-responsive">
-                    <div id="odometer"></div>
-                  </div>
-                <!-- /.table-responsive -->
-              </div>
-            </div>
-          </div>
-         </div>
-       </div>
+      
         
  </section>
 </div>
