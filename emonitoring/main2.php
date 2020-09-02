ec1fd44acad662d0a063d045bbb605d54e08733a
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-02 19:56:31 +0800

    Update main2.php

--- emonitoring/main2.php
+++ emonitoring/main2.php
@@ -277,7 +277,6 @@ $a = 1;
         <div class="row mb-2">
           <div class="col-sm-6">
             <h1 class="m-0 text-dark" style="font-family: 'Fugaz One', cursive;">eSPBT 2.0 System Dashboard</h1>
-            <p>(*All data shown below as of <?php echo $date;?>)</p>
           </div><!-- /.col -->
           <div class="col-sm-6">
             <ol class="breadcrumb float-sm-right">
