3d4f83ed295f00a20c16c3e23fad8bc0e94feb95
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-02 21:28:50 +0800

    Update main3.php

--- emonitoring/main3.php
+++ emonitoring/main3.php
@@ -475,26 +475,28 @@ $a = 1;
      
 		});
 </script>
-<script type="text/javascript">
-        function myFunction() {
-        document.getElementById("myDropdown").classList.toggle("show");
-      }
+<script>
+/* When the user clicks on the button,
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
 
-      function filterFunction() {
-        var input, filter, ul, li, a, i;
-        input = document.getElementById("myInput");
-        filter = input.value.toUpperCase();
-        div = document.getElementById("myDropdown");
-        a = div.getElementsByTagName("a");
-        for (i = 0; i < a.length; i++) {
-          txtValue = a[i].textContent || a[i].innerText;
-          if (txtValue.toUpperCase().indexOf(filter) > -1) {
-            a[i].style.display = "";
-          } else {
-            a[i].style.display = "none";
-          }
-        }
-      }
+function filterFunction() {
+  var input, filter, ul, li, a, i;
+  input = document.getElementById("myInput");
+  filter = input.value.toUpperCase();
+  div = document.getElementById("myDropdown");
+  a = document.getElementById("myInput2");
+  for (i = 0; i < a.length; i++) {
+    txtValue = a[i].textContent || a[i].innerText;
+    if (txtValue.toUpperCase().indexOf(filter) > -1) {
+      a[i].style.display = "";
+    } else {
+      a[i].style.display = "none";
+    }
+  }
+}
 </script>
 <!-- DataTables -->
 <script src="../adminSPBT/plugins/datatables/jquery.dataTables.js"></script>
