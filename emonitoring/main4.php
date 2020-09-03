5a91bdce3284105a94a27d5167cd47c278d2400d
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-03 20:28:00 +0800

    Update main4.php

--- emonitoring/main4.php
+++ emonitoring/main4.php
@@ -289,7 +289,7 @@ $a = 1;
       </div><!-- /.container-fluid -->
     </div>
    
-      <section class="content" id="print">
+      <section class="content" id="content">
         <div id="row">
         <div class="col-md-12">
            <!-- TABLE: list of publisherSPBT -->
@@ -469,7 +469,8 @@ $a = 1;
                           </table>
                                 <input type="hidden" name="kodSekolah" value="<?php echo $dataSekolah['kodSekolah'];?>">
                                 <div class="modal-footer">
-                                   <button class="btn btn-info" onclick="javascript:demoFromHTML();">Cetak</button>
+                                   <div id="editor"></div>
+                                   <button class="btn btn-info" id="cmd">Cetak</button>
                                 </div>
                           
                         </div>
@@ -584,45 +585,20 @@ $a = 1;
 </script>
 <script type="text/javascript" src="jspdf.min.js"></script>
 <script>
- function demoFromHTML() {
-    var pdf = new jsPDF('p', 'pt', 'letter');
-    // source can be HTML-formatted string, or a reference
-    // to an actual DOM element from which the text will be scraped.
-    source = $('#print')[0];
-
-    // we support special element handlers. Register them with jQuery-style 
-    // ID selector for either ID or node name. ("#iAmID", "div", "span" etc.)
-    // There is no support for any other type of selectors 
-    // (class, of compound) at this time.
-    specialElementHandlers = {
-        // element with id of "bypass" - jQuery style selector
-        '#bypassme': function (element, renderer) {
-            // true = "handled elsewhere, bypass text extraction"
-            return true
+ var doc = new jsPDF();
+    var specialElementHandlers = {
+        '#editor': function (element, renderer) {
+            return true;
         }
     };
-    margins = {
-        top: 80,
-        bottom: 60,
-        left: 40,
-        width: 522
-    };
-    // all coords and widths are in jsPDF instance's declared units
-    // 'inches' in this case
-    pdf.fromHTML(
-    source, // HTML string or DOM elem ref.
-    margins.left, // x coord
-    margins.top, { // y coord
-        'width': margins.width, // max width of content on PDF
-        'elementHandlers': specialElementHandlers
-    },
 
-    function (dispose) {
-        // dispose: object with X, Y of the last line add to the PDF 
-        //          this allow the insertion of new lines after html
-        pdf.save('laporan_pemantauan.pdf');
-    }, margins);
-}
+    $('#cmd').click(function () {
+        doc.fromHTML($('#content').html(), 15, 15, {
+            'width': 170,
+                'elementHandlers': specialElementHandlers
+        });
+        doc.save('laporan_pemantauan.pdf');
+    });
 </script>
 <script>
 /* When the user clicks on the button,
