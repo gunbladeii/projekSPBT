4cef11376f04cdd2b8397f23f09520d132058ce6
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-03 17:35:01 +0800

    Update dashboard.php

--- emonitoring/dashboard.php
+++ emonitoring/dashboard.php
@@ -368,7 +368,7 @@ $a = 1;
                               <?php do {?>
                               <tr>
                                 <td><?php echo $a++;?></td>
-                                <td><a href="main3.php?kodSekolah=<?php echo $dataSekolah['kodSekolah'];?>"><span class="badge badge-info"><?php echo strtoupper($dataSekolah['kodSekolah']);?></span></a></td>
+                                <td><a href="main4.php?kodSekolah=<?php echo $dataSekolah['kodSekolah'];?>"><span class="badge badge-info"><?php echo strtoupper($dataSekolah['kodSekolah']);?></span></a></td>
                                 <td><?php echo $dataSekolah['namaSekolah'];?></td>
                                 <td><?php echo strtoupper($dataSekolah['negeri']);?></td>
                                 <td><i class="far fa-check-circle"></i></td>
