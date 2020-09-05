3702106ecf4fbe41f32d744095c4af05a199f337
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-05 10:59:32 +0800

    Update main3.php

--- emonitoring/main3.php
+++ emonitoring/main3.php
@@ -474,7 +474,7 @@ $a = 1;
                           </div>
                       <?php ;}else {echo 'Tiada dalam rekod';}?>
 
-                      <?php if(!empty($dataJudul2)) {?>
+                      <?php if($dataJudul2 > 0) {?>
                       <div class="table-responsive">
                         <table class="table table-sm">
                           <thead>
@@ -493,7 +493,7 @@ $a = 1;
                           </tbody>
                         </table>
                       </div>
-                      <?php ;} else {echo '<span class="badge badge-warning">Tiada padanan yang sesuai,Sila cuba carian semula</td><span>'}?>
+                      <?php ;} ?>
 
                       <?php if($rekodPemantauan > 0) {?>
                         <div class="table-responsive">
