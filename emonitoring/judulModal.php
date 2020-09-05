6269417f1bf86b7187006fec427529b8bc6e67f3
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-05 14:22:29 +0800

    Update judulModal.php

--- emonitoring/judulModal.php
+++ emonitoring/judulModal.php
@@ -80,7 +80,7 @@
 <!--calculate total earning, deduction, grand total
 <script src="../calculateTotalSalary.js"></script>-->
 
-<script src="https://code.jquery.com/jquery-3.5.1.js" integrity="sha256-QWo7LDvxbWT2tbbQ97B53yJnYU3WhH/C8ycbRAkjPDc=" crossorigin="anonymous"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
 <script>
     $(document).ready(function() {
     //this calculates values automatically 
@@ -97,7 +97,7 @@
             var result = parseInt(num1) - parseInt(num2);
             if (!isNaN(result)) 
             {
-        document.getElementById('bukuStok').value = result;
+              document.getElementById('bukuStok').value = result;
             }
            
         }
