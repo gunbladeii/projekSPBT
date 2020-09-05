88657795eb0a138e532dd8072297396d0f380eca
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-05 14:04:56 +0800

    Update judulModal.php

--- emonitoring/judulModal.php
+++ emonitoring/judulModal.php
@@ -53,7 +53,7 @@
                                     <div class="form-group"> 
                                         Jumlah Stok:
                                        <div class="input-group mb-3">
-                                         <input type="text" id="bukuStok" name="bukuStok" value="" required>
+                                         <input type="text" id="bukuStok" name="bukuStok" class="form-control" value="" required>
                                          <div class="input-group-append input-group-text">
                                           <span class="fas fa-id-card-alt"></span>
                                          </div>
