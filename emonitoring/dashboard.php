414f76a2f711d7b41ee130b23912c69a471c88f6
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-06 20:53:48 +0800

    Update dashboard.php

--- emonitoring/dashboard.php
+++ emonitoring/dashboard.php
@@ -27,7 +27,7 @@ $Recordset = $mysqli->query("SELECT * FROM login WHERE username = '$colname_Reco
 $row_Recordset = mysqli_fetch_assoc($Recordset);
 $totalRows_Recordset = mysqli_num_rows($Recordset);
 
-$Recordset2 = $mysqli->query("SELECT * FROM dataSekolah WHERE remark LIKE 'observe'");
+$Recordset2 = $mysqli->query("SELECT * FROM dataSekolah WHERE remark = 'observe'");
 $dataSekolah = mysqli_fetch_assoc($Recordset2);
 $totalRows_Recordset2 = mysqli_num_rows($Recordset2);
 
