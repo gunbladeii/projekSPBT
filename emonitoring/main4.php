7ec3aa8639a98867455c0dc8ac207484e0fb3588
gunbladeii~fisha <53096103+gunbladeii@users.noreply.github.com>
2020-09-08 11:41:38 +0800

    Update main4.php

--- emonitoring/main4.php
+++ emonitoring/main4.php
@@ -590,8 +590,8 @@ function filterFunction() {
   var printWindow = window.open('', '', 'height=400,width=700');
   printWindow.document.write('<html><head><title>*Cetakan digital. Tiada tandatangan diperlukan.</title>');
   printWindow.document.write('</head><body >');
-  printWindow.document.write(divContents);
   printWindow.document.write('<img src="logo_kpm.png" class="rounded mx-auto d-block">');
+  printWindow.document.write(divContents);
   printWindow.document.write('</body></html>');
   printWindow.document.close();
   printWindow.print();
